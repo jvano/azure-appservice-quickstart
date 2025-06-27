f9f239d8fd4cd0af91bb336fd0dcfeff4f5e8c17
Joaquín Vano ☕🏀 <joaquinv@microsoft.com>
2025-06-26 18:26:34 -0700

    dotnet http1-tls

---
+++ windows-containers/dotnet/http1-tls-8.0-nanoserver-ltsc2022/build.cmd
@@ -0,0 +1,1 @@
+docker build --isolation=hyperv -t joaquinvcr.azurecr.io/dotnet:http1-tls-8.0-nanoserver-ltsc2022 .
---
+++ windows-containers/dotnet/http1-tls-8.0-nanoserver-ltsc2022/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace dotnetapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.ListenAnyIP(port: 443, listenOptions =>
+                        {
+                            var cert = new X509Certificate2("app.pfx", "changeit", X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+
+                            listenOptions.UseHttps(cert);
+                            listenOptions.Protocols = HttpProtocols.Http1;
+                        });
+                    });
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
