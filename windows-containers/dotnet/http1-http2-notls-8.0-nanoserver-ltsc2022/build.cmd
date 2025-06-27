4aaf65e07fe3e570338d2d6d4c40eff0419fe980
Joaquín Vano ☕🏀 <joaquinv@microsoft.com>
2025-06-26 18:30:11 -0700

    dotnet http1-http2-notls

---
+++ windows-containers/dotnet/http1-http2-notls-8.0-nanoserver-ltsc2022/build.cmd
@@ -0,0 +1,1 @@
+docker build --isolation=hyperv -t joaquinvcr.azurecr.io/dotnet:http1-http2-notls-8.0-nanoserver-ltsc2022 .
---
+++ windows-containers/dotnet/http1-http2-notls-8.0-nanoserver-ltsc2022/run.cmd
@@ -0,0 +1,1 @@
+docker run -d --isolation=hyperv joaquinvcr.azurecr.io/dotnet:http1-http2-notls-8.0-nanoserver-ltsc2022
